9b4a9bb7daa7b4fd324a843884302b4f64e7a0fd
abolsen <6937877+abolsen@users.noreply.github.com>
2021-07-30 17:17:55 +0200

    Update install_boinc.sh

--- install_boinc.sh
+++ install_boinc.sh
@@ -2,8 +2,8 @@
 sudo apt-get update -y && sudo apt-get upgrade -y
 #install libs and boinc
 sudo apt-get install lib32z1 libstdc++6 freeglut3 boinc-client -y
-#join boincstats
-boinccmd --join_acct_mgr http://bam.boincstats.com [myuser] [mypassword]
+#join boincstats (weak authentication - below is not an account password, it merely let's anyone join computers to a specific account)
+boinccmd --join_acct_mgr http://bam.boincstats.com abolsen 241003_d87f9510554144e6ca8fce0f87929670
 #start boinc
 sudo /etc/init.d/boinc-client start
 #start boinc at boot time
