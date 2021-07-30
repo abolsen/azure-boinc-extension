83ca1a4fc14c38e5736289fb4696b17317b67059
abolsen <6937877+abolsen@users.noreply.github.com>
2021-07-30 13:27:57 +0200

    Update install_boinc.sh

--- install_boinc.sh
+++ install_boinc.sh
@@ -1,14 +1,12 @@
 #update packages
 sudo apt-get update -y && sudo apt-get upgrade -y
 #install libs and boinc
-sudo apt-get install ia32-libs libstdc++6 freeglut3 boinc-client
-#install libstdc++5 on late Ubuntu dists
-wget http://mirrors.kernel.org/ubuntu/pool/universe/g/gcc-3.3/libstdc++5_3.3.6-17ubuntu1_i386.deb
-sudo dpkg-deb -x libstdc++5_3.3.6-17ubuntu1_i386.deb ./tmp 
-sudo cp ./tmp/usr/lib/* /usr/lib32/
+sudo apt-get install lib32z1 libstdc++6 freeglut3 boinc-client -y
 #join boincstats
 boinccmd --join_acct_mgr http://bam.boincstats.com [myuser] [mypassword]
 #start boinc
-/etc/init.d/boinc-client start
+sudo /etc/init.d/boinc-client start
 #start boinc at boot time
-update-rc.d boinc-client defaults 98
+sudo update-rc.d boinc-client defaults 98
+#install boinctui
+sudo apt-get install boinctui -y
