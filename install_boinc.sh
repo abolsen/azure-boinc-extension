7d5ba2bd01228c028cf6d9ba43e54fedb8395530
abolsen <6937877+abolsen@users.noreply.github.com>
2021-08-01 15:20:52 +0200

    Update install_boinc.sh

--- install_boinc.sh
+++ install_boinc.sh
@@ -1,12 +1,10 @@
 #update packages
 sudo apt-get update -y && sudo apt-get upgrade -y
-#install libs and boinc
-sudo apt-get install lib32z1 libstdc++6 freeglut3 boinc-client -y
+#install stuff
+sudo apt-get install lib32z1 libstdc++6 freeglut3 boinc-client boinctui virtualbox -y
 #join boincstats (weak authentication - below is not an account password, it merely let's anyone join computers to a specific account)
 boinccmd --join_acct_mgr http://bam.boincstats.com 241003_d87f9510554144e6ca8fce0f87929670
 #start boinc
 sudo /etc/init.d/boinc-client start
 #start boinc at boot time
 sudo update-rc.d boinc-client defaults 98
-#install boinctui
-sudo apt-get install boinctui -y
